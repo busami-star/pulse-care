b5ff7d22f5c74b8418660fa6fcd9c8df4e5d23aa
busami-star <victorbusami1@gmail.com>
2025-01-17 15:12:03 +0300

    homepage commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,10 +1,12 @@
 import logo from './logo.jpeg';
 import profile_pic from './profile_pic.jpeg'
 import group_profiles from './group_profiles.png'
+import header_image from './header_image.jpeg'
 
 
 export const assets = {
     logo,
     profile_pic,
     group_profiles,
+    header_image,
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,35 @@
-
-
+import { assets } from "../assets/assets";
 const Header = () => {
   return (
     <div>
-        <div>
-            <p>Connect with trusted specialists <br />and schedule your visit effortlessly.</p>
-        </div>
-        <div></div>
-    </div>
-  )
-}
+      <div className="flex md:items-center lg:items-start flex-col-reverse md:flex-row justify-between">
+        <div className="py-8 pl-0">
+          <h1 className="text-3xl pb-4 md:text-4xl lg:text-5xl font-semibold">
+            Find a doctor, schedule an appointment
+            <br /> and experience the difference.
+          </h1>
+          <p className="pb-6">
+            Healing is not just a destination—it’s a journey that unfolds one
+            step at a time.
+            <br /> Every small improvement, no matter how insignificant it may
+            seem,
+            <br /> is a sign of progress and a reason to hold onto hope.{" "}
+          </p>
 
-export default Header
+          <div className="flex   gap-2">
+            <img src={assets.group_profiles} alt="" />
+            <a className="hover:scale-105 transition-all duration-200 rounded-full bg-primary py-4 px-3" href="">
+              schedule appointment
+            </a>
+          </div>
+        </div>
+
+        <div className="w-[60%] ">
+          <img className="w-full" src={assets.header_image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
